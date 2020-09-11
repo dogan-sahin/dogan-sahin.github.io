aa2ebf3ff25b09d914759ec351ab09918cfcd151
dokksen <dogansahin80@gmailcom>
2020-09-11 17:05:00 +0200

    change some text in about me

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
 
+$(document).ready(function () {
 
-
-$(document).ready(function(){
-
-    $("a").on('click', function(event) {
+    $("a").on('click', function (event) {
         if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
 
-        event.preventDefault();
-        var hash = this.hash;
-
-        $('html, body').animate({
-        scrollTop: $(hash).offset().top
-        }, 800, function(){
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
 
                 window.location.hash = hash;
-        });
-        } 
+            });
+        }
     });
 
-    $(".dropdown").click(function()
-    {
+    $(".dropdown").click(function () {
 
-        if($(".title").is(':visible'))
-        {
-        $(".navigation_list").slideDown(500);
-        $(".title").css("display","none"); 
+        if ($(".title").is(':visible')) {
+            $(".navigation_list").slideDown(500);
+            $(".title").css("display", "none");
         }
 
-        else
-        {
-        $(".navigation_list").slideUp(1);
-        $(".title").css("display","block");
-        
-        }   
+        else {
+            $(".navigation_list").slideUp(1);
+            $(".title").css("display", "block");
+
+        }
     });
 
 });
 
 
-window.addEventListener('scroll', function() {
-	var element = document.querySelector('.skillbox');
-	var position = element.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+    var element = document.querySelector('.skillbox');
+    var position = element.getBoundingClientRect();
 
-	// checking whether fully visible
-	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+    // checking whether fully visible
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
         $('.bar span').addClass("fillspan");
     }
-    
-    // checking for partial visibility
-	/*if(position.top < window.innerHeight && position.bottom >= 0) {
-        $('.bar span').addClass("fillspan");
-	}*/
 });
 
 
